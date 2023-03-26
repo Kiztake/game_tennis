33e6789525154851486eb680063143685fbe4259
Kiztake <128293169+Kiztake@users.noreply.github.com>
2023-03-26 14:59:44 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -16,22 +16,23 @@ class Player(GameSprite):
         keys = key.get_pressed()
         if keys[K_UP] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[K_DOWN] and self.rect.y < win_height - 180:
+        if keys[K_DOWN] and self.rect.y < win_height - 140:
             self.rect.y += self.speed
     def update_l(self):
         keys = key.get_pressed()
         if keys[K_w] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[K_s] and self.rect.y <  win_height - 180:
+        if keys[K_s] and self.rect.y < win_height - 140:
             self.rect.y += self.speed
         
 back = (200, 255, 255)
 win_width = 600
 win_height = 500
 window = display.set_mode((win_width, win_height))
+display.set_caption("Game ping-pong")
 
-racket1 = Player("racket.png", 30, win_height//2, 4, 50, 150)
-racket2 = Player("racket.png", 520, win_height//2, 4, 50, 150)
+racket1 = Player("racket.png", 30, win_height//2, 4, 40, 120)
+racket2 = Player("racket.png", 520, win_height//2, 4, 40, 120)
 ball = GameSprite('tenis_ball.png', 200, win_height//2, 4, 50, 50)
 
 font.init()
@@ -39,8 +40,8 @@ font = font.Font(None, 35)
 lose1 = font.render("Player 1 lose!!!", True, (180, 0, 0))
 lose2 = font.render("Player 2 lose!!!", True, (180, 0, 0))
 
-speed_x = 3
-speed_y = 3
+v_x = 3
+v_y = 2
 
 game = True
 finish = False
@@ -52,13 +53,33 @@ while game:
         if e.type == QUIT:
             game = False
 
-    window.fill(back)
+    
     if not finish:   
+        window.fill(back)
+
         racket1.update_l()
         racket1.reset()
 
         racket2.update_r()
         racket2.reset()
+
+        ball.reset()
+        ball.rect.x += v_x
+        ball.rect.y += v_y
+
+        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+            v_x *= -1
+
+
+        if ball.rect.y >= win_height - 50 or ball.rect.y <= 50:
+            v_y *= -1
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(lose1, (200, 200))
+        elif ball.rect.x >= win_width:
+            finish = True
+            window.blit(lose2, (200, 200))
+
 
 
     display.update()
